db8c869359f0d9f1d7597df9acb751785989086b
Jhonatan Oliveira <jhonatan.oliveira931002@gmail.com>
2023-03-10 13:18:48 -0300

    :lock: feat: tratamento de variáveis de ambiente e criação de .env

--- main.go
+++ main.go
@@ -3,19 +3,24 @@ package main
 import (
 	"database/sql"
 	"log"
+	"os"
 
 	"github.com/jhonatan-oliveiradev/go_finance_backend/api"
 	db "github.com/jhonatan-oliveiradev/go_finance_backend/db/sqlc"
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://postgres:postgres@localhost:5432/go_finance?sslmode=disable"
-	serverAddress = "0.0.0.0:8000"
-)
-
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	dbDriver := os.Getenv("DB_DRIVER")
+	dbSource := os.Getenv("DB_SOURCE")
+	serverAddress := os.Getenv("SERVER_ADDRESS")
+
 	conn, err := sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal("cannot connect to db: ", err)
